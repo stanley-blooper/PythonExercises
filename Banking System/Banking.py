25781ed6d1f764a6575fa28df9229a782cea8f28
stanley-blooper <60666266+stanley-blooper@users.noreply.github.com>
2024-09-29 21:28:18 -0400

    Create Banking.py

---
+++ Banking System/Banking.py
@@ -0,0 +1,51 @@
+balance = 0
+
+def show_menu():
+    print("Welcome to Jean-Baptiste Bank!")
+    print("1. Chck balance")
+    print("2. Deposit Money")
+    print("3. Withdraw Money")
+    print("4. Exit")
+
+balance=0
+while True:
+    show_menu()
+    
+    
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        print(f"Your balance is ${balance}")
+
+        continu =input("Will that be all? Y or N")
+        if continu == 'N':
+             show_menu()
+        if continu == 'Y':
+             print("Have a nice day! :)")
+    elif choice == '2':
+        deposit=float(input("How much money would you like to deposit? "))
+        if deposit > 0:
+            balance += deposit
+            print(f"Deposit was  successful! New balance is ${balance}")
+        elif deposit < 0:
+            print("Cannot deposit a negative balance. Back to main menu.")
+    elif choice == '3':
+        withdrawal=float(input(f"How much money would you like to withdraw? "))
+        if withdrawal <= balance and withdrawal >0:
+                balance -= withdrawal
+                print(f"")
+        else:
+             print(f"Insufficient funds. You only have ${balance} in your account")
+        
+    elif choice == '4':
+        print("Have a nice day! :)")
+        break
+
+
+    
+
+
+    #commenting
+
+
+    
